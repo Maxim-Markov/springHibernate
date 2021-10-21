9aa561ca7faaf1ad674b43c7448b1132598fe81c
Maxim-Markov <maxxmarkov@gmail.com>
2021-10-21 16:05:29 +0300

    implementation commit

--- src/main/java/hiber/dao/UserDaoImp.java
+++ src/main/java/hiber/dao/UserDaoImp.java
@@ -1,5 +1,6 @@
 package hiber.dao;
 
+import hiber.model.Car;
 import hiber.model.User;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,19 +12,38 @@ import java.util.List;
 @Repository
 public class UserDaoImp implements UserDao {
 
-   @Autowired
-   private SessionFactory sessionFactory;
+    private final SessionFactory sessionFactory;
 
-   @Override
-   public void add(User user) {
-      sessionFactory.getCurrentSession().save(user);
-   }
+    @Autowired
+    public UserDaoImp(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
 
-   @Override
-   @SuppressWarnings("unchecked")
-   public List<User> listUsers() {
-      TypedQuery<User> query=sessionFactory.getCurrentSession().createQuery("from User");
-      return query.getResultList();
-   }
+    @Override
+    public void add(User user) {
+        sessionFactory.getCurrentSession().save(user);
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public List<User> listUsers() {
+        TypedQuery<User> query = sessionFactory.getCurrentSession().createQuery("from User");
+        return query.getResultList();
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public User getUserbyCar(Car car) {
+        TypedQuery<Car> query = null;
+        // if(car.getUser() == null) {
+        query = sessionFactory.getCurrentSession().createQuery("from Car where model = :carModel and series = :carSeries");
+        query.setParameter("carModel", car.getModel());
+        query.setParameter("carSeries", car.getSeries());
+        // }
+
+        TypedQuery<User> queryUser = sessionFactory.getCurrentSession().createQuery("from User where id = :idByCar");
+        queryUser.setParameter("idByCar", query != null ? query.getSingleResult().getUser().getId() : car.getUser().getId());
+        return queryUser.getSingleResult();
+    }
 
 }
---
+++ src/main/java/hiber/model/Car.java
@@ -0,0 +1,55 @@
+package hiber.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "cars")
+public class Car {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    public Car(String model, String series) {
+        this.model = model;
+        this.series = series;
+    }
+
+    private String model;
+
+    private String series;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    public Car() {
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getSeries() {
+        return series;
+    }
+
+    public void setSeries(String series) {
+        this.series = series;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Long getId() {
+        return id;
+    }
+}
